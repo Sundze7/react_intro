d6a329502d953d2f495edb6a4976b08092d66dfb
Sundze <keyonmwesundze@mail.com>
2024-08-25 07:56:50 +0100

    favorites page items

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,25 @@
+import { useContext } from "react";
+
+import FavoritesContext from "../store/favorite-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
-  return <div>Favorites Page</div>;
+  const favoriteCtx = useContext(FavoritesContext);
+
+  let content;
+
+  if (favoriteCtx.totalFavorites === 0) {
+    content = <p>You got no favorites yet. So, start adding some</p>;
+  } else {
+    content = <MeetupList meetups={favoriteCtx.favorites} />;
+  }
+
+  return (
+    <section>
+      <h1>My Favorites</h1>
+      {content}
+    </section>
+  );
 }
 
 export default FavoritesPage;
